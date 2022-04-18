4ba0650f99a438a42234787e72f934875399b372
beiyuouo <beiyu233@gmail.com>
2022-04-19 00:47:10 +0800

    update

--- README.md
+++ README.md
@@ -13,7 +13,9 @@ to install the dependendies of the voxel renderer.
 Please fill in your code in `main.py` and include your result in this README.md file.
 
 ## Demo
-![](./demo.jpg)
+
+![](./images/demo1.jpg)
+![](./images/demo2.jpg)
 
 ## Quickstart
 
@@ -23,7 +25,6 @@ python3 example1.py  # example2/3/4.py
 
 Mouse and keyboard interface:
 
-+ Drag with your left mouse button to rotate camera.
-+ Press `W/A/S/D/Q/E` to move camera.
-+ Press `P` to save screenshot.
-
+- Drag with your left mouse button to rotate camera.
+- Press `W/A/S/D/Q/E` to move camera.
+- Press `P` to save screenshot.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cmath import cos
+import numpy as np
 from scene import Scene
 import taichi as ti
 from taichi.math import *
@@ -6,6 +6,13 @@ from taichi.math import *
 scene = Scene(exposure=1)
 scene.set_directional_light((1, 1, 1), 0.1, (1, 1, 1))
 scene.set_background_color((0.3, 0.4, 0.6))
+
+
+@ti.func
+def rotate(v, axis, theta):
+    c = ti.cos(theta)
+    s = ti.sin(theta)
+    return v * c + axis * (1 - c) * v.dot(axis) + s * (v.cross(axis))
 
 
 @ti.kernel
@@ -18,20 +25,33 @@ def initialize_voxels():
     vec_z = vec3(0, 1, 0)
     vec_normal = normalize(cross(vec_face_to, vec_z))
 
-    mouse_angle = pi / 4
-    mouse_angle_cos = ti.cos(mouse_angle)
-    skin_thickness = 1
+    mouse_angle = pi / 5
+    mouse_angle_min = mouse_angle
+    mouse_angle_max = mouse_angle
+    mouse_angle_cos_min = ti.cos(mouse_angle_min) - 0.05
+    mouse_angle_cos_max = ti.cos(mouse_angle_max) + 0.05
+    skin_thickness = 0.5
+
+    eye_angle = mouse_angle + pi / 18
+    he_angle = pi / 5
+
+    vec_eye_left = rotate(rotate(vec_face_to, vec_normal, -eye_angle), vec_z, he_angle).normalized()
+    vec_eye_right = rotate(rotate(vec_face_to, vec_normal, -eye_angle), vec_z,
+                           -he_angle).normalized()
+
+    p_eye_left = p_center + vec_eye_left * r
+    p_eye_right = p_center + vec_eye_right * r
+    eye_size = 3
 
     # scene.set_voxel(p_center, 2, vec3(1, 1, 1))
 
     for i, j, k in ti.ndrange((-n, n), (-n, n), (-n, n)):
         x = ivec3(i, j, k)
-
+        color = vec3(2, 2, 2)
         # surface
         if distance(x, p_center) < r + skin_thickness and distance(x,
                                                                    p_center) > r - skin_thickness:
 
-            color = vec3(2, 2, 2)
             # mouse
             # project to the plane
             vec_mouse = vec3(i, j, k) - p_center
@@ -39,14 +59,30 @@ def initialize_voxels():
             print(vec_mouse_projected)
 
             # angle to face to
-            angle = dot(vec_mouse_projected,
-                        vec_face_to) / (vec_mouse_projected.norm() * vec_face_to.norm())
+            angle_cos = dot(vec_mouse_projected,
+                            vec_face_to) / (vec_mouse_projected.norm() * vec_face_to.norm())
 
-            if angle <= mouse_angle_cos:
+            if angle_cos <= mouse_angle_cos_max:
                 color = vec3(1, 1, 0.3)
 
-            if any(color != vec3(2, 2, 2)):
-                scene.set_voxel(vec3(i, j, k), 1, color)
+            if distance(vec3(i, j, k), p_eye_left) <= eye_size or distance(
+                    vec3(i, j, k), p_eye_right) <= eye_size:
+                color = vec3(0.01, 0.01, 0.01)
+
+        elif distance(x, p_center) <= r - skin_thickness:
+            # mouse
+            # project to the plane
+            vec_mouse = vec3(i, j, k) - p_center
+            vec_mouse_projected = vec_mouse - vec_normal * dot(vec_mouse, vec_normal)
+            print(vec_mouse_projected)
+
+            angle_cos = dot(vec_mouse_projected,
+                            vec_face_to) / (vec_mouse_projected.norm() * vec_face_to.norm())
+
+            if mouse_angle_cos_min <= angle_cos and angle_cos <= mouse_angle_cos_max:
+                color = vec3(0.01, 0.01, 0.01)
+        if any(color != vec3(2, 2, 2)):
+            scene.set_voxel(vec3(i, j, k), 1, color)
 
 
 initialize_voxels()
